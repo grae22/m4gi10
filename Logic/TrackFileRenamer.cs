5a786a36b217cb2001d5af499750e485bd525aa7
graeme <grae22@gmail.com>
2018-05-03 17:02:02 +0000

    Missed from prev commit.

---
+++ Logic/TrackFileRenamer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace m4gi10.Logic
+{
+  internal static class TrackFileRenamer
+  {
+    //---------------------------------------------------------------------------------------------
+
+    public static IEnumerable<RenamedTrackFile> GetRenamedTrackFiles(IEnumerable<ITrackFile> trackFiles)
+    {
+      var renamedTrackFiles = new List<RenamedTrackFile>();
+
+      foreach (ITrackFile trackFile in trackFiles)
+      {
+        var newName = $"{trackFile.Artist.Replace(" ", "")}_{trackFile.TrackNumber}";
+
+        renamedTrackFiles.Add(new RenamedTrackFile(trackFile, newName));
+      }
+
+      return renamedTrackFiles;
+    }
+
+    //---------------------------------------------------------------------------------------------
+  }
+}
