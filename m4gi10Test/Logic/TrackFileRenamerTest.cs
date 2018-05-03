beb0f16258a9f99ac64d10b7c6c33bfd192b263e
graeme <grae22@gmail.com>
2018-05-03 17:01:51 +0000

    Some initial renaming logic.

--- m4gi10Test/Logic/TrackFileRenamerTest.cs
+++ m4gi10Test/Logic/TrackFileRenamerTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using m4gi10.Logic;
 
 namespace m4gi10Test.Logic
 {
@@ -12,11 +16,16 @@ namespace m4gi10Test.Logic
     public void GetRenamedTrackFiles_GivenOneTrackFile_ShouldReturnRenamedTrackFile()
     {
       // Arrange.
-      //var 
+      var trackFile = Substitute.For<ITrackFile>();
+      trackFile.Artist.Returns("Some Artist");
+      trackFile.TrackNumber.Returns(1);
 
       // Act.
+      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(new List<ITrackFile> { trackFile });
 
       // Assert.
+      Assert.AreEqual(1, renamedFiles.Count());
+      Assert.AreEqual("SomeArtist_1", renamedFiles.First().NewFilename);
     }
 
     //---------------------------------------------------------------------------------------------
