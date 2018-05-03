fe03f7aaf983e1b2c9785add27ad78ec60fade4a
graeme <grae22@gmail.com>
2018-05-03 15:59:09 +0000

    Adding projecs and some initial TrackFile stuff.

---
+++ TrackFile.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace m4gi10
+{
+  internal class TrackFile
+  {
+    //---------------------------------------------------------------------------------------------
+
+    public string Artist { get; private set; } = "";
+    public int TrackNumber { get; private set; }
+
+    private const string FilePropertyAlbumArtist = "System.Music.AlbumArtist";
+    private const string FilePropertyArtist = "System.Music.Artist";
+    private const string FilePropertyTrackNumber = "System.Music.TrackNumber";
+
+    private string Filename { get; }
+    private IFileSystem FileSystem { get; }
+    private IFileExtendedPropertyRetriever FilePropertyRetriever { get; }
+
+    //---------------------------------------------------------------------------------------------
+
+    public TrackFile(
+      string filename,
+      IFileSystem fileSystem,
+      IFileExtendedPropertyRetriever filePropertyRetriever)
+    {
+      Filename = filename;
+      FileSystem = fileSystem;
+      FilePropertyRetriever = filePropertyRetriever;
+
+      ValidateFileExists();
+      UpdateArtist();
+      UpdateTrackNumber();
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private void ValidateFileExists()
+    {
+      if (FileSystem.File.Exists(Filename))
+      {
+        return;
+      }
+
+      throw new ArgumentException($"File does not exist: \"{Filename}\"");
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private void UpdateArtist()
+    {
+      Artist = GetAlbumArtistPropertyValue();
+
+      if (!string.IsNullOrWhiteSpace(Artist))
+      {
+        return;
+      }
+
+      Artist = GetArtistPropertyValue();
+
+      if (!string.IsNullOrWhiteSpace(Artist))
+      {
+        return;
+      }
+
+      Artist = "Unknown";
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private string GetAlbumArtistPropertyValue()
+    {
+      return FilePropertyRetriever.GetPropertyValue(FilePropertyAlbumArtist);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private string GetArtistPropertyValue()
+    {
+      return FilePropertyRetriever.GetPropertyValue(FilePropertyArtist);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private void UpdateTrackNumber()
+    {
+      string value = FilePropertyRetriever.GetPropertyValue(FilePropertyTrackNumber);
+
+      if (string.IsNullOrWhiteSpace(value))
+      {
+        return;
+      }
+
+      if (!int.TryParse(value, out int trackNumber))
+      {
+        return;
+      }
+
+      TrackNumber = trackNumber;
+    }
+
+    //---------------------------------------------------------------------------------------------
+  }
+}
---
+++ m4gi10Test/TrackFileTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO.Abstractions;
+using NUnit.Framework;
+using m4gi10;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace m4gi10Test
+{
+  [TestFixture]
+  public class TrackFileTest
+  {
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFilenameThatDoesntExist_ShouldThrowException()
+    {
+      // Arrange.
+      const string invalidFilename = "someFileThatDoesntExist.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(invalidFilename).Returns(false);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+
+      // Act & Assert.
+      Assert.Throws<ArgumentException>(
+        () =>
+          new TrackFile(invalidFilename, fileSystem, filePropertyRetriever));
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFileWithoutArtistAttrib_ShouldReturnUnknownForArtist()
+    {
+      // Arrange.
+      const string validFilename = "someFile.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(validFilename).Returns(true);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+      filePropertyRetriever.GetPropertyValue("System.Music.Artist").Returns("");
+
+      // Act.
+      var testObject = new TrackFile(validFilename, fileSystem, filePropertyRetriever);
+
+      // Assert.
+      Assert.AreEqual("Unknown", testObject.Artist);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFileWithArtistAttrib_ShouldReturnValueForArtist()
+    {
+      // Arrange.
+      const string validFilename = "someFile.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(validFilename).Returns(true);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+      filePropertyRetriever.GetPropertyValue("System.Music.Artist").Returns("SomeArtist");
+
+      // Act.
+      var testObject = new TrackFile(validFilename, fileSystem, filePropertyRetriever);
+
+      // Assert.
+      Assert.AreEqual("SomeArtist", testObject.Artist);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFileWithoutArtistAndWithAlbumArtistAttrib_ShouldReturnAlbumArtistValueForArtist()
+    {
+      // Arrange.
+      const string validFilename = "someFile.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(validFilename).Returns(true);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+      filePropertyRetriever.GetPropertyValue("System.Music.Artist").Returns("SomeArtist");
+      filePropertyRetriever.GetPropertyValue("System.Music.AlbumArtist").Returns("SomeOtherArtist");
+
+      // Act.
+      var testObject = new TrackFile(validFilename, fileSystem, filePropertyRetriever);
+
+      // Assert.
+      Assert.AreEqual("SomeOtherArtist", testObject.Artist);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFileWithoutTrackNumberAttrib_ShouldReturnZero()
+    {
+      // Arrange.
+      const string validFilename = "someFile.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(validFilename).Returns(true);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+      filePropertyRetriever.GetPropertyValue("System.Music.TrackNumber").Returns("");
+
+      // Act.
+      var testObject = new TrackFile(validFilename, fileSystem, filePropertyRetriever);
+
+      // Assert.
+      Assert.AreEqual(0, testObject.TrackNumber);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFile")]
+    public void Constructor_GivenFileWithTrackNumberAttrib_ShouldReturnValue()
+    {
+      // Arrange.
+      const string validFilename = "someFile.txt";
+
+      var fileSystem = Substitute.For<IFileSystem>();
+      fileSystem.File.Exists(validFilename).Returns(true);
+
+      var filePropertyRetriever = Substitute.For<IFileExtendedPropertyRetriever>();
+      filePropertyRetriever.GetPropertyValue("System.Music.TrackNumber").Returns("10");
+
+      // Act.
+      var testObject = new TrackFile(validFilename, fileSystem, filePropertyRetriever);
+
+      // Assert.
+      Assert.AreEqual(10, testObject.TrackNumber);
+    }
+
+    //---------------------------------------------------------------------------------------------
+  }
+}
