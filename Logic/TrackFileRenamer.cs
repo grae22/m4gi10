30eaad7a53613f3e9799a68e1485679141eca264
GraemeB <graeme.bruschi@derivco.co.za>
2018-05-04 12:42:34 +0000

    TrackFileRenamer logic mostly done.

--- Logic/TrackFileRenamer.cs
+++ Logic/TrackFileRenamer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace m4gi10.Logic
 {
@@ -6,18 +8,113 @@ namespace m4gi10.Logic
   {
     //---------------------------------------------------------------------------------------------
 
-    public static IEnumerable<RenamedTrackFile> GetRenamedTrackFiles(IEnumerable<ITrackFile> trackFiles)
+    public static IEnumerable<RenamedTrackFile> GetRenamedTrackFiles(
+      IEnumerable<ITrackFile> trackFiles,
+      int artistNameMaxLength = 6,
+      int albumNameMaxLength = 6)
     {
-      var renamedTrackFiles = new List<RenamedTrackFile>();
+      var renamedTracks = new List<RenamedTrackFile>();
+      var albumsByArtist = GetAlbumsByArtist(trackFiles);
 
-      foreach (ITrackFile trackFile in trackFiles)
+      foreach (var artist in albumsByArtist.Keys)
       {
-        var newName = $"{trackFile.Artist.Replace(" ", "")}_{trackFile.TrackNumber}";
-
-        renamedTrackFiles.Add(new RenamedTrackFile(trackFile, newName));
+        AddRenamedAlbumTracksToList(
+          artist,
+          albumsByArtist[artist],
+          trackFiles,
+          renamedTracks,
+          artistNameMaxLength,
+          albumNameMaxLength);
       }
 
-      return renamedTrackFiles;
+      return renamedTracks.OrderBy(rt => rt.NewFilename);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private static IDictionary<string, List<string>> GetAlbumsByArtist(IEnumerable<ITrackFile> tracks)
+    {
+      var albumsByArtist = new Dictionary<string, List<string>>();
+
+      foreach (var track in tracks)
+      {
+        var artist = track.Artist;
+        var album = track.Album;
+
+        if (!albumsByArtist.ContainsKey(artist))
+        {
+          albumsByArtist.Add(artist, new List<string>());
+        }
+
+        if (albumsByArtist[artist].Contains(album))
+        {
+          continue;
+        }
+
+        albumsByArtist[artist].Add(album);
+      }
+
+      return albumsByArtist;
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private static void AddRenamedAlbumTracksToList(
+      string artist,
+      List<string> albums,
+      IEnumerable<ITrackFile> trackFiles,
+      List<RenamedTrackFile> renamedTracks,
+      int artistNameMaxLength,
+      int albumNameMaxLength)
+    {
+      foreach (var album in albums)
+      {
+        var albumTracks =
+          trackFiles
+            .Where(a =>
+              a.Artist.Equals(artist, StringComparison.OrdinalIgnoreCase) &&
+              a.Album.Equals(album, StringComparison.OrdinalIgnoreCase))
+            .OrderBy(t => t.TrackNumber);
+
+        int trackNumber = 1;
+
+        foreach (var track in albumTracks)
+        {
+          RenameTrackAndAddToList(
+            track,
+            trackNumber++,
+            renamedTracks,
+            artistNameMaxLength,
+            albumNameMaxLength);
+        }
+      }
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    private static void RenameTrackAndAddToList(
+      ITrackFile track,
+      int trackNumber,
+      List<RenamedTrackFile> renamedTracks,
+      int artistNameMaxLength,
+      int albumNameMaxLength)
+    {
+      var newArtistName = $"{track.Artist.Replace(" ", "")}";
+      var newAlbum = $"{track.Album.Replace(" ", "")}";
+
+      if (newArtistName.Length > artistNameMaxLength)
+      {
+        newArtistName = newArtistName.Substring(0, artistNameMaxLength);
+      }
+
+      if (newAlbum.Length > albumNameMaxLength)
+      {
+        newAlbum = newAlbum.Substring(0, albumNameMaxLength);
+      }
+
+      var newName = $"{newArtistName}_{newAlbum}_{trackNumber}";
+
+      renamedTracks.Add(new RenamedTrackFile(track, newName));
     }
 
     //---------------------------------------------------------------------------------------------
--- m4gi10Test/Logic/TrackFileRenamerTest.cs
+++ m4gi10Test/Logic/TrackFileRenamerTest.cs
@@ -18,14 +18,163 @@ namespace m4gi10Test.Logic
       // Arrange.
       var trackFile = Substitute.For<ITrackFile>();
       trackFile.Artist.Returns("Some Artist");
+      trackFile.Album.Returns("Some Album");
       trackFile.TrackNumber.Returns(1);
 
       // Act.
-      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(new List<ITrackFile> { trackFile });
+      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(
+        new List<ITrackFile> { trackFile },
+        99,
+        99);
 
       // Assert.
       Assert.AreEqual(1, renamedFiles.Count());
-      Assert.AreEqual("SomeArtist_1", renamedFiles.First().NewFilename);
+      Assert.AreEqual("SomeArtist_SomeAlbum_1", renamedFiles.First().NewFilename);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFileRenamer")]
+    public void GetRenamedTrackFiles_GivenMultipleArtists_ShouldReturnInOrder()
+    {
+      // Arrange.
+      var trackFile1 = Substitute.For<ITrackFile>();
+      trackFile1.Artist.Returns("Some Artist");
+      trackFile1.TrackNumber.Returns(1);
+
+      var trackFiles = new List<ITrackFile>
+      {
+        Substitute.For<ITrackFile>(),
+        Substitute.For<ITrackFile>(),
+        Substitute.For<ITrackFile>(),
+        Substitute.For<ITrackFile>()
+      };
+
+      trackFiles[0].Artist.Returns("Some Artist 2");
+      trackFiles[1].Artist.Returns("Some Artist 1");
+      trackFiles[2].Artist.Returns("Some Artist 1");
+      trackFiles[3].Artist.Returns("Some Artist 2");
+
+      trackFiles[0].Album.Returns("Some Album");
+      trackFiles[1].Album.Returns("Some Album");
+      trackFiles[2].Album.Returns("Some Album");
+      trackFiles[3].Album.Returns("Some Album");
+
+      // Act.
+      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(
+        trackFiles,
+        99,
+        99);
+      
+      // Assert.
+      Assert.AreEqual("SomeArtist1_SomeAlbum_1", renamedFiles.ElementAt(0).NewFilename);
+      Assert.AreEqual("SomeArtist1_SomeAlbum_2", renamedFiles.ElementAt(1).NewFilename);
+      Assert.AreEqual("SomeArtist2_SomeAlbum_1", renamedFiles.ElementAt(2).NewFilename);
+      Assert.AreEqual("SomeArtist2_SomeAlbum_2", renamedFiles.ElementAt(3).NewFilename);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFileRenamer")]
+    public void GetRenamedTrackFiles_GivenMultipleArtistsAndAlbums_ShouldReturnInOrderByArtistAndAlbum()
+    {
+      // Arrange.
+      var artist1Album1Track1 = Substitute.For<ITrackFile>();
+      var artist1Album1Track2 = Substitute.For<ITrackFile>();
+      var artist1Album2Track1 = Substitute.For<ITrackFile>();
+      var artist1Album2Track2 = Substitute.For<ITrackFile>();
+      var artist2Album1Track1 = Substitute.For<ITrackFile>();
+      var artist2Album1Track2 = Substitute.For<ITrackFile>();
+
+      artist1Album1Track1.Artist.Returns("Artist1");
+      artist1Album1Track2.Artist.Returns("Artist1");
+      artist1Album2Track1.Artist.Returns("Artist1");
+      artist1Album2Track2.Artist.Returns("Artist1");
+      artist2Album1Track1.Artist.Returns("Artist2");
+      artist2Album1Track2.Artist.Returns("Artist2");
+
+      artist1Album1Track1.Album.Returns("1");
+      artist1Album1Track2.Album.Returns("1");
+      artist1Album2Track1.Album.Returns("2");
+      artist1Album2Track2.Album.Returns("2");
+      artist2Album1Track1.Album.Returns("3");
+      artist2Album1Track2.Album.Returns("3");
+
+      var trackFiles = new List<ITrackFile>
+      {
+        artist2Album1Track2,
+        artist2Album1Track1,
+        artist1Album2Track2,
+        artist1Album2Track1,
+        artist1Album1Track2,
+        artist1Album1Track1
+      };
+      
+      // Act.
+      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(
+        trackFiles,
+        99,
+        99);
+
+      // Assert.
+      Assert.AreEqual("Artist1_1_1", renamedFiles.ElementAt(0).NewFilename);
+      Assert.AreEqual("Artist1_1_2", renamedFiles.ElementAt(1).NewFilename);
+      Assert.AreEqual("Artist1_2_1", renamedFiles.ElementAt(2).NewFilename);
+      Assert.AreEqual("Artist1_2_2", renamedFiles.ElementAt(3).NewFilename);
+      Assert.AreEqual("Artist2_3_1", renamedFiles.ElementAt(4).NewFilename);
+      Assert.AreEqual("Artist2_3_2", renamedFiles.ElementAt(5).NewFilename);
+    }
+
+    //---------------------------------------------------------------------------------------------
+
+    [Test]
+    [Category("TrackFileRenamer")]
+    public void GetRenamedTrackFiles_GivenLongArtistAndAlbumNames_ShouldTruncate()
+    {
+      // Arrange.
+      var artist1Album1Track1 = Substitute.For<ITrackFile>();
+      var artist1Album1Track2 = Substitute.For<ITrackFile>();
+      var artist1Album2Track1 = Substitute.For<ITrackFile>();
+      var artist1Album2Track2 = Substitute.For<ITrackFile>();
+      var artist2Album1Track1 = Substitute.For<ITrackFile>();
+      var artist2Album1Track2 = Substitute.For<ITrackFile>();
+
+      artist1Album1Track1.Artist.Returns("Some Artist1");
+      artist1Album1Track2.Artist.Returns("Some Artist1");
+      artist1Album2Track1.Artist.Returns("Some Artist1");
+      artist1Album2Track2.Artist.Returns("Some Artist1");
+      artist2Album1Track1.Artist.Returns("Some Artist2");
+      artist2Album1Track2.Artist.Returns("Some Artist2");
+
+      artist1Album1Track1.Album.Returns("Some Album 1");
+      artist1Album1Track2.Album.Returns("Some Album 1");
+      artist1Album2Track1.Album.Returns("Some Album 2");
+      artist1Album2Track2.Album.Returns("Some Album 2");
+      artist2Album1Track1.Album.Returns("Some Album 3");
+      artist2Album1Track2.Album.Returns("Some Album 3");
+
+      var trackFiles = new List<ITrackFile>
+      {
+        artist2Album1Track2,
+        artist2Album1Track1,
+        artist1Album2Track2,
+        artist1Album2Track1,
+        artist1Album1Track2,
+        artist1Album1Track1
+      };
+
+      // Act.
+      var renamedFiles = TrackFileRenamer.GetRenamedTrackFiles(trackFiles);
+
+      // Assert.
+      Assert.AreEqual("SomeAr_SomeAl_1", renamedFiles.ElementAt(0).NewFilename);
+      Assert.AreEqual("SomeAr_SomeAl_2", renamedFiles.ElementAt(1).NewFilename);
+      Assert.AreEqual("SomeAr_SomeAl_1", renamedFiles.ElementAt(2).NewFilename);
+      Assert.AreEqual("SomeAr_SomeAl_2", renamedFiles.ElementAt(3).NewFilename);
+      Assert.AreEqual("SomeAr_SomeAl_1", renamedFiles.ElementAt(4).NewFilename);
+      Assert.AreEqual("SomeAr_SomeAl_2", renamedFiles.ElementAt(5).NewFilename);
     }
 
     //---------------------------------------------------------------------------------------------
